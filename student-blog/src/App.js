9dadacce00a264bdd1b21a107bc72af27a1cf0db
Langster2323 <langahkeem@gmail.com>
2020-02-07 06:31:13 -0600

    Deleted useless files. Developed state within app component

--- student-blog/src/App.js
+++ student-blog/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      students: [
+        {firstName: 'Ahkeem', lastName: 'Lang', age: 27, hobbies: 'Sand Volleyball, RTS video games, bouldering, running, kickboxing, breakdancing, reading, traveling'},
+        {firstName: 'Albert', lastName: 'Wash', age: 44, hobbies: 'Walking dog, watching James Bond movies'},
+        {firstName: 'Becky', lastName: 'Chen', age: 33, hobbies: 'Sand Volleyball, kickboxing, reading, listening to podcast, playing with puppy'},
+        {firstName: 'John', lastName: 'Hob', age: 25, hobbies: 'Learning Japanese, kite flying, bouldering'},
+        {firstName: 'Steve', lastName: 'James', age: 32, hobbies: 'Indoor Volleyball, video games, bouldering'},
+        {firstName: 'Jake', lastName: 'Fang', age: 41, hobbies: 'Football, RTS video games, bouldering'},
+        {firstName: 'Jenyy', lastName: 'Colbolt', age: 53, hobbies: 'Soccer, RTS video games, hiking'},
+        {firstName: 'Jude', lastName: 'Beatle', age: 62, hobbies: 'Baseball, board games, bouldering'},
+        {firstName: 'Ali', lastName: 'Long', age: 29, hobbies: 'Rugby, board games, bouldering'},
+        {firstName: 'Alison', lastName: 'Long', age: 25, hobbies: 'Sand Volleyball, RTS video games, hiking'}
+      ]
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
